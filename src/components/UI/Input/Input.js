296957806d83d2c8c6c7c8f70f704beaacb8c02c
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-06-27 01:49:03 +0300

    создал компонент инпут

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+function isInValid(valid, touched, validate) {
+  return !valid && touched && validate;
+}
+
+const Input = (props) => {
+  const inputId = `${props.type}-${Math.random()}`;
+  const inputType = props.type || 'text';
+  const cls = [classes.Input];
+  if (props.class) {
+    cls.push(props.class);
+  }
+  return (
+    <div className={cls.join(' ')}>
+      <label for={inputId}>{props.name}</label>
+      <input id={inputId} type={inputType} onChange={props.onChange} value={props.value} />
+      {isInValid(props) ? <span>{props.errorMessage || 'Введите верное значение'}</span> : null}
+    </div>
+  );
+};
+
+export default Input;
