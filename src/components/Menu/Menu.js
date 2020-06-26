c0b95b968ad3ccda1b7c43ecfb92035a3eb888ea
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-06-27 01:49:58 +0300

    Внес изменения в Меню

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,12 @@
 import React from "react";
 import classes from "./Menu.module.css";
+import { NavLink } from "react-router-dom";
+
+const link = [
+  { to: "/", name: "Активный тест", exact: true },
+  { to: "/auth", name: "Авторизация", exact: false },
+  { to: "/create-quiz", name: "Создать тест", exact: false },
+];
 
 const Menu = (props) => {
   const cls = [classes.Menu];
@@ -8,7 +15,24 @@ const Menu = (props) => {
   }
   return (
     <React.Fragment>
-      <nav className={cls.join(" ")}></nav>
+      <nav className={cls.join(" ")}>
+        <ul>
+          {link.map((elem) => {
+            return (
+              <li>
+                <NavLink
+                  to={elem.to}
+                  exact={elem.exact}
+                  activeClassName={classes.active}
+                  onClick={props.onClick}
+                >
+                  {elem.name}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </React.Fragment>
   );
 };
