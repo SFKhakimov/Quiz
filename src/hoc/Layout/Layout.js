dbc60f9d90f8045207a91aad0fcc114159d3ab77
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-06-26 22:58:00 +0300

    Создал компонент высокого порядка. Он отвечает за выдчау контента на странице

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import classes from "./Layout.module.css";
+import Menu from "../../components/Menu/Menu";
+import MenuToggle from "../../components/MenuToggle/MenuToggle";
+import Overlay from "../../components/UI/Overlay/Overlay";
+
+export default class Layout extends Component {
+  state = {
+    menuIsOpen: false,
+  };
+
+  clickHandler = () => {
+    this.setState(() => {
+      return {
+        menuIsOpen: !this.state.menuIsOpen,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className={classes.Layout}>
+        <Menu menuIsOpen={this.state.menuIsOpen} />
+        {this.state.menuIsOpen ? <Overlay onClick={this.clickHandler} /> : null}
+        <MenuToggle
+          menuIsOpen={this.state.menuIsOpen}
+          onClick={this.clickHandler}
+        />
+        <main>{this.props.children}</main>
+      </div>
+    );
+  }
+}
