27d9f3f3a2a2f50f5bbba14e3988970baba85511
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-06-27 23:01:03 +0300

    Сделал рабочики кнопки Повторить и Перейти ко всем тестам

--- src/components/FinishQuiz/FinishQuiz.js
+++ src/components/FinishQuiz/FinishQuiz.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./FinishQuiz.module.css";
 import Button from "../UI/Button/Button";
+import { NavLink } from "react-router-dom";
 
 const FinishQuiz = (props) => {
   const resultsQuiz = Object.keys(props.resultsQuiz).reduce((total, index) => {
@@ -26,8 +27,12 @@ const FinishQuiz = (props) => {
         })}
       </ul>
       <div>
-        <Button type="FinishQuiz_repeat">Повторить</Button>
-        <Button type="FinishQuiz_all-test">Перейти ко всем тестам</Button>
+        <Button type="FinishQuiz_repeat" onClick={props.onClick}>
+          Повторить
+        </Button>
+        <NavLink to="/">
+          <Button type="FinishQuiz_all-test">Перейти ко всем тестам</Button>
+        </NavLink>
       </div>
     </div>
   );
