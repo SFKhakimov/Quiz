080e0ed9567c2ddd84c135fc1f7d89f5a867c70d
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-06-27 23:01:58 +0300

    Настроил динамийский роутинг. Чтобы тест можно было открывать из списка тестов

--- src/container/Quiz/Quiz.js
+++ src/container/Quiz/Quiz.js
@@ -2,36 +2,16 @@ import React, { Component } from "react";
 import classes from "./Quiz.module.css";
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 import FinishQuiz from "../../components/FinishQuiz/FinishQuiz";
+import Loader from "../../components/UI/Loader/Loader";
+import axios from "axios";
 
 export default class Quiz extends Component {
   state = {
     resultsQuiz: {},
-    optionsAnswer: null, // success или error
+    optionsAnswer: null,
     currentQuestion: 0,
-    quiz: [
-      {
-        id: 1,
-        correctAnswer: 3,
-        question: "В каком году основали Москву?",
-        answer: [
-          { options: "1084г", id: 1 },
-          { options: "1094г", id: 2 },
-          { options: "1147г", id: 3 },
-          { options: "1244г", id: 4 },
-        ],
-      },
-      {
-        id: 2,
-        correctAnswer: 2,
-        question: "Кто освободил Русь от Монголов?",
-        answer: [
-          { options: "Иван Купала", id: 1 },
-          { options: "Иван Грозный", id: 2 },
-          { options: "Артем Великий", id: 3 },
-          { options: "Святослав Всеволодович ", id: 4 },
-        ],
-      },
-    ],
+    quiz: [],
+    loading: true,
   };
 
   handleClick = (answerId) => {
@@ -74,27 +54,57 @@ export default class Quiz extends Component {
     }, 1000);
   };
 
+  repeat = () => {
+    this.setState(() => {
+      return {
+        currentQuestion: 0,
+      };
+    });
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await axios.get(
+        `https://quiz-39d4e.firebaseio.com/quiz/${this.props.match.params.id}.json`
+      );
+      const quiz = res.data;
+      this.setState(() => {
+        return {
+          quiz,
+          loading: false,
+        };
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   render() {
     return (
       <div className={classes.Quiz}>
         <h1>Ответьте на все вопросы</h1>
-        <div>
-          {this.state.currentQuestion === this.state.quiz.length ? (
-            <FinishQuiz
-              quizLength={this.state.quiz.length}
-              resultsQuiz={this.state.resultsQuiz}
-              quiz={this.state.quiz}
-            />
-          ) : (
-            <ActiveQuiz
-              value={this.state.quiz[this.state.currentQuestion]}
-              onClick={this.handleClick}
-              styleState={this.state.optionsAnswer}
-              quizLength={this.state.quiz.length}
-              currentQuestion={this.state.currentQuestion + 1}
-            />
-          )}
-        </div>
+        {this.state.loading ? (
+          <Loader />
+        ) : (
+          <div>
+            {this.state.currentQuestion === this.state.quiz.length ? (
+              <FinishQuiz
+                quizLength={this.state.quiz.length}
+                resultsQuiz={this.state.resultsQuiz}
+                quiz={this.state.quiz}
+                onClick={this.repeat}
+              />
+            ) : (
+              <ActiveQuiz
+                value={this.state.quiz[this.state.currentQuestion]}
+                onClick={this.handleClick}
+                styleState={this.state.optionsAnswer}
+                quizLength={this.state.quiz.length}
+                currentQuestion={this.state.currentQuestion + 1}
+              />
+            )}
+          </div>
+        )}
       </div>
     );
   }
