d95dd5d2ca7aeeb9f4212ea961e03f67979bb001
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-06-27 13:25:57 +0300

    Добавил классы для страницы авторизации

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Button.module.css";
 
 const Button = (props) => {
-  const cls = [classes.Button, classes[props.style]];
+  const cls = [classes.Button, classes[props.type]];
   return (
     <button
       className={cls.join(" ")}
