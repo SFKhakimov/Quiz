e4188107069930cd92322247cfc75730ebda6ff8
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-06-28 02:08:57 +0300

    Updates

---
+++ precache-manifest.ae04cdcfb2cb9246dd283639f2bd59de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6ebcecc02f9829e1b6b79f9fd8f39d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6442243add3f4f41e453",
+    "url": "/static/css/main.556264e5.chunk.css"
+  },
+  {
+    "revision": "2dcc68ffbcc84e8613e0",
+    "url": "/static/js/2.201f9631.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.201f9631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6442243add3f4f41e453",
+    "url": "/static/js/main.7f833931.chunk.js"
+  },
+  {
+    "revision": "df7d820ded1d4db0ff91",
+    "url": "/static/js/runtime-main.410ddc6b.js"
+  }
+]);
