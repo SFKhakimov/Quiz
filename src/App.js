ed04202197870480888d3042d6b1e2432f4fe25e
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-06-27 16:32:11 +0300

    Немного поправил верстку и инициализировал в App компонент QuizCreate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import Quiz from "./container/Quiz/Quiz";
 import Auth from "./container/Auth/Auth";
+import QuizCreate from "./container/QuizCreate/QuizCreate";
 import { Route } from "react-router-dom";
 
 export default class App extends Component {
@@ -14,6 +15,9 @@ export default class App extends Component {
           </Route>
           <Route path="/auth">
             <Auth />
+          </Route>
+          <Route path="/create-quiz">
+            <QuizCreate />
           </Route>
         </Layout>
       </div>
