9d4f154291a702c78b59e8c11d82d7ccbb1550ac
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-06-27 13:27:20 +0300

    Реализовал форму авторизации

--- src/container/Auth/Auth.js
+++ src/container/Auth/Auth.js
@@ -1,44 +1,98 @@
-import React, { Component } from 'react';
-import classes from './Auth.module.css';
-import Input from '../../components/UI/Input/Input';
+import React, { Component } from "react";
+import classes from "./Auth.module.css";
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+import { checkValidate } from "../../utils/form/form";
 
 export default class Auth extends Component {
   state = {
     formValid: false,
     formsElements: {
       email: {
-        value: '',
-        label: 'Email',
-        type: 'email',
-        touche: false,
+        value: "",
+        label: "Email",
+        type: "email",
+        touched: false,
         valid: false,
-        errorMessage: 'Введите корректный email',
+        errorMessage: "Введите корректный email",
         validate: {
           required: true,
           email: true,
         },
-        password: {
-          value: '',
-          label: 'Пароль',
-          type: 'password',
-          touche: false,
-          valid: false,
-          errorMessage: 'Введите корректный пароль',
-          validate: {
-            required: true,
-            minLength: 6,
+      },
+      password: {
+        value: "",
+        label: "Пароль",
+        type: "password",
+        touched: false,
+        valid: false,
+        errorMessage: "Введите корректный пароль",
+        validate: {
+          required: true,
+          minLength: 6,
         },
       },
     },
-  },
+  };
+
+  changeHandler = (event, inputName) => {
+    const input = { ...this.state.formsElements[inputName] };
+    const form = { ...this.state.formsElements };
+
+    input.value = event.target.value;
+    input.touched = true;
+    input.valid = checkValidate(input.value, input.validate);
+
+    form[inputName] = input;
+
+    let formIsValid = true;
+
+    Object.keys(this.state.formsElements).forEach((elem) => {
+      const input = this.state.formsElements[elem];
+      if (input.valid) {
+        formIsValid = input.valid && formIsValid;
+      }
+    });
+
+    this.setState(() => {
+      return {
+        formsElements: form,
+        formValid: formIsValid,
+      };
+    });
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div className={classes.Auth}>
         <div>
           <h2>Авторизоваться</h2>
-          <form>
-            <Input name={this.state.label}></Input>
-            <Input name={this.state.label}></Input>
+          <form onSubmit={this.submitHandler}>
+            {Object.keys(this.state.formsElements).map((elem, index) => {
+              const input = this.state.formsElements[elem];
+              return (
+                <Input
+                  name={input.label}
+                  type={input.type}
+                  value={input.value}
+                  errorMessage={input.errorMessage}
+                  valid={input.valid}
+                  touched={input.touched}
+                  onChange={(event) => this.changeHandler(event, elem)}
+                  key={input + index}
+                ></Input>
+              );
+            })}
+            <Button disabled={!this.state.formValid} type="signin">
+              Авторизоваться
+            </Button>
+            <Button disabled={!this.state.formValid} type="signup">
+              Зарегистрироваться
+            </Button>
           </form>
         </div>
       </div>
