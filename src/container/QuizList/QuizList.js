2a47fddb7e4bbc5e7b06d207edc2bdaf40859583
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-07-08 02:12:57 +0300

    Сделал компонент функциональным. Начал использовать хуки, состояние беру из props(store)

--- src/container/QuizList/QuizList.js
+++ src/container/QuizList/QuizList.js
@@ -1,58 +1,74 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import classes from "./QuizList.module.css";
 import Loader from "../../components/UI/Loader/Loader";
-import axios from "axios";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  fetchQuizListStart,
+  fetchQuizListEnd,
+  fetchQuizListError,
+} from "../../redux/action/quizList";
+import axios from "axios";
 
-export default class QuizList extends Component {
-  state = {
-    quizList: [],
-    loading: true,
-  };
+const QuizList = (props) => {
+  const { fetchQuizListStart, fetchQuizListEnd, fetchQuizListError } = props;
 
-  async componentDidMount() {
-    try {
-      const res = await axios.get(
-        "https://quiz-39d4e.firebaseio.com/quiz.json"
-      );
-      const quizList = [];
-      Object.keys(res.data).forEach((elem, index) => {
-        quizList.push({
-          id: elem,
-          name: `Тест №${index + 1}`,
+  useEffect(() => {
+    async function fetchQuizList() {
+      try {
+        fetchQuizListStart();
+        const res = await axios.get(
+          "https://quiz-39d4e.firebaseio.com/quiz.json"
+        );
+        const quizList = [];
+        Object.keys(res.data).forEach((elem, index) => {
+          quizList.push({
+            id: elem,
+            name: `Тест №${index + 1}`,
+          });
         });
-      });
-      this.setState(() => {
-        return {
-          quizList,
-          loading: false,
-        };
-      });
-    } catch (err) {
-      console.log(err);
+        fetchQuizListEnd(quizList);
+      } catch (err) {
+        fetchQuizListError(err);
+      }
     }
-  }
+    fetchQuizList();
+  }, [fetchQuizListStart, fetchQuizListEnd, fetchQuizListError]);
+  return (
+    <div className={classes.QuizList}>
+      {props.loading ? (
+        <Loader type="position" />
+      ) : (
+        <div>
+          <h2>Список тестов</h2>
+          <ul>
+            {props.quizList.map((quiz) => {
+              return (
+                <li key={quiz.id}>
+                  <NavLink to={`/quiz/${quiz.id}`}>{quiz.name}</NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className={classes.QuizList}>
-        {this.state.loading ? (
-          <Loader type="position" />
-        ) : (
-          <div>
-            <h2>Список тестов</h2>
-            <ul>
-              {this.state.quizList.map((quiz) => {
-                return (
-                  <li key={quiz.id}>
-                    <NavLink to={`/quiz/${quiz.id}`}>{quiz.name}</NavLink>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
-      </div>
-    );
-  }
+function mapStateToProps(state) {
+  return {
+    quizList: state.quizList.quizList,
+    loading: state.quizList.loading,
+  };
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchQuizListStart: () => dispatch(fetchQuizListStart()),
+    fetchQuizListEnd: (quizList) => dispatch(fetchQuizListEnd(quizList)),
+    fetchQuizListError: (err) => dispatch(fetchQuizListError(err)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizList);
