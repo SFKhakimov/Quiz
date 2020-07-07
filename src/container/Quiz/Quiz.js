ef8ce538087a63456ab04c46868eb1058d4df4f3
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-07-08 02:11:52 +0300

    переписал компонент Квиз. Убрал использование класса, использовал хуки и вынес состояние в store

--- src/container/Quiz/Quiz.js
+++ src/container/Quiz/Quiz.js
@@ -1,111 +1,89 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import classes from "./Quiz.module.css";
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 import FinishQuiz from "../../components/FinishQuiz/FinishQuiz";
 import Loader from "../../components/UI/Loader/Loader";
 import axios from "axios";
+import { connect } from "react-redux";
+import {
+  handleClick,
+  repeat,
+  fetchQuizStart,
+  fetchQuizError,
+  fetchQuizSetState,
+} from "../../redux/action/quiz";
 
-export default class Quiz extends Component {
-  state = {
-    resultsQuiz: {},
-    optionsAnswer: null,
-    currentQuestion: 0,
-    quiz: [],
-    loading: true,
-  };
+const Quiz = (props) => {
+  const { fetchQuizSetState, fetchQuizError, fetchQuizStart } = props;
 
-  handleClick = (answerId) => {
-    if (this.state.optionsAnswer) {
-      const key = Object.keys(this.state.optionsAnswer)[0];
-      if (this.state.optionsAnswer[key] === "success" || "error") {
-        return;
+  useEffect(() => {
+    fetchQuizStart();
+    async function fetchQuiz() {
+      try {
+        const res = await axios.get(
+          `https://quiz-39d4e.firebaseio.com/quiz/${props.match.params.id}.json`
+        );
+        const quiz = res.data;
+        fetchQuizSetState(quiz);
+      } catch (err) {
+        fetchQuizError(err);
       }
     }
+    fetchQuiz();
+  }, [
+    fetchQuizSetState,
+    fetchQuizError,
+    fetchQuizStart,
+    props.match.params.id,
+  ]);
 
-    const question = this.state.quiz[this.state.currentQuestion];
-    const result = { ...this.state.resultsQuiz };
+  return (
+    <div className={classes.Quiz}>
+      <h1>Ответьте на все вопросы</h1>
+      {props.loading ? (
+        <Loader />
+      ) : (
+        <div>
+          {props.currentQuestion === props.quiz.length ? (
+            <FinishQuiz
+              quizLength={props.quiz.length}
+              resultsQuiz={props.resultsQuiz}
+              quiz={props.quiz}
+              onClick={props.repeat}
+            />
+          ) : (
+            <ActiveQuiz
+              value={props.quiz[props.currentQuestion]}
+              onClick={props.handleClick}
+              styleState={props.optionsAnswer}
+              quizLength={props.quiz.length}
+              currentQuestion={props.currentQuestion + 1}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
-    if (answerId === question.correctAnswer) {
-      result[this.state.currentQuestion] = "success";
-      this.setState(() => {
-        return {
-          optionsAnswer: { [answerId]: "success" },
-          resultsQuiz: result,
-        };
-      });
-    } else {
-      result[this.state.currentQuestion] = "error";
-      this.setState(() => {
-        return {
-          optionsAnswer: { [answerId]: "error" },
-          resultsQuiz: result,
-        };
-      });
-    }
-
-    const timeout = setTimeout(() => {
-      this.setState((state) => {
-        return {
-          currentQuestion: state.currentQuestion + 1,
-          optionsAnswer: null,
-        };
-      });
-      window.setTimeout(timeout);
-    }, 1000);
+function mapStateToProps(state) {
+  return {
+    resultsQuiz: state.quiz.resultsQuiz,
+    optionsAnswer: state.quiz.optionsAnswer,
+    currentQuestion: state.quiz.currentQuestion,
+    quiz: state.quiz.quiz,
+    loading: state.quiz.loading,
   };
-
-  repeat = () => {
-    this.setState(() => {
-      return {
-        currentQuestion: 0,
-      };
-    });
-  };
-
-  async componentDidMount() {
-    try {
-      const res = await axios.get(
-        `https://quiz-39d4e.firebaseio.com/quiz/${this.props.match.params.id}.json`
-      );
-      const quiz = res.data;
-      this.setState(() => {
-        return {
-          quiz,
-          loading: false,
-        };
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  render() {
-    return (
-      <div className={classes.Quiz}>
-        <h1>Ответьте на все вопросы</h1>
-        {this.state.loading ? (
-          <Loader />
-        ) : (
-          <div>
-            {this.state.currentQuestion === this.state.quiz.length ? (
-              <FinishQuiz
-                quizLength={this.state.quiz.length}
-                resultsQuiz={this.state.resultsQuiz}
-                quiz={this.state.quiz}
-                onClick={this.repeat}
-              />
-            ) : (
-              <ActiveQuiz
-                value={this.state.quiz[this.state.currentQuestion]}
-                onClick={this.handleClick}
-                styleState={this.state.optionsAnswer}
-                quizLength={this.state.quiz.length}
-                currentQuestion={this.state.currentQuestion + 1}
-              />
-            )}
-          </div>
-        )}
-      </div>
-    );
-  }
 }
+
+function mapDispathToProps(dispatch) {
+  return {
+    handleClick: (answerId) => dispatch(handleClick(answerId)),
+    repeat: () => dispatch(repeat()),
+    fetchQuizSetState: (quiz) => dispatch(fetchQuizSetState(quiz)),
+    fetchQuizStart: () => dispatch(fetchQuizStart()),
+    fetchQuizError: (err) => dispatch(fetchQuizError(err)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispathToProps)(Quiz);
